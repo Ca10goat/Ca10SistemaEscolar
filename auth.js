70276a0765be4d4fcafb574f15d0ae734eb88d85
Ca10goat <cmaua2012@gmail.com>
2026-04-26 20:13:34 -0300

    Update auth.js

--- auth.js
+++ auth.js
@@ -1,22 +1,12 @@
 import { 
   getAuth, 
-  createUserWithEmailAndPassword, 
   signInWithEmailAndPassword,
   onAuthStateChanged
 } from "https://www.gstatic.com/firebasejs/12.12.1/firebase-auth.js";
 
 const auth = getAuth(window.firebaseApp);
 
-// REGISTRAR
-window.registrar = function(email, senha) {
-  createUserWithEmailAndPassword(auth, email, senha)
-    .then((userCredential) => {
-      console.log("Usuário criado:", userCredential.user.email);
-    })
-    .catch((error) => {
-      console.log("Erro:", error.message);
-    });
-};
+
 
 // LOGIN
 window.login = function(email, senha) {
